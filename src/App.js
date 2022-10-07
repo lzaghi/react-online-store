d44a4e58c404dc4c32a9ba2c418cadc663430021
Miguel <m.angelodev@gmail.com>
2022-10-07 15:17:03 -0300

    Formata o modelo do App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="Site">
+        <p>Site</p>
+      </div>
+    );
+  }
 }
-
-export default App;
