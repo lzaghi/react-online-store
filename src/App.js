1663fc9fba930ec6ade48bf93cd39c98bbbda8ff
Miguel <m.angelodev@gmail.com>
2022-10-07 16:06:38 -0300

    Adiciona função getProductById e chama ela no App.js para imprimir no console.log

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Component } from 'react';
-import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
+import {
+  getCategories,
+  getProductById,
+  getProductsFromCategoryAndQuery,
+} from './services/api';
 
 export default class App extends Component {
   render() {
     // Estou chamando a função aqui, para dar o console.log do array voltado para a requisição da API -Miguel
-    getCategories();
-    getProductsFromCategoryAndQuery('MLB1743', 'biz');
+    getCategories().then();
+    getProductsFromCategoryAndQuery('MLB1743', 'biz').then();
+    getProductById('MLB2784514906').then();
     return (
       <div className="Site">
         <p>Site</p>
