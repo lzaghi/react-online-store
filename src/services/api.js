1fbc4744c7d380a27abcd2f34ea06c464d43b34b
Miguel <m.angelodev@gmail.com>
2022-10-07 15:37:00 -0300

    Adiciona função getProductsFromCategoryAndQuery e a roda de teste em App.js

--- src/services/api.js
+++ src/services/api.js
@@ -8,7 +8,15 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-
+  const CATEGORY = categoryId;
+  const SEARCH = query;
+  const ENDPOINT = `https://api.mercadolibre.com/sites/MLB/search?category=$${CATEGORY}&q=$${SEARCH}`;
+  const REQUEST = await fetch(ENDPOINT);
+  const RESPONSE = await REQUEST.json();
+  // Apague o console.log caso necessário. -Miguel
+  // A chave que contém resultado na busca é o .results -Miguel
+  console.log(RESPONSE);
+  return RESPONSE;
 }
 
 export async function getProductById() {
