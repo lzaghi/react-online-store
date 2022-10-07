ca23a7c9944c1684d5f39b64f9b3e45d952f36fc
Miguel <m.angelodev@gmail.com>
2022-10-07 18:40:17 -0300

    Adicona componente flexivel para criação das categorias, reformula SearchInput

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,23 +1,45 @@
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 export default class SearchInput extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    getCategories().then((results) => {
+      this.setState({
+        categories: results,
+      });
+    });
+  }
+
   render() {
+    // const { categories } = this.state;
     return (
-      <div>
-        <input type="text" />
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-        <Link to="/cart">
-          <button
-            type="button"
-            data-testid="shopping-cart-button"
-          >
-            carrinho
-          </button>
-        </Link>
-      </div>
+      <>
+        <div>
+          <input type="text" />
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </div>
+        <div>
+          <Link to="/cart">
+            <button
+              type="button"
+              data-testid="shopping-cart-button"
+            >
+              carrinho
+            </button>
+          </Link>
+        </div>
+      </>
     );
   }
 }
