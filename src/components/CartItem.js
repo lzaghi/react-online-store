e1af201103c41b37ff9617b718b8437b4f12dc4f
Miguel <m.angelodev@gmail.com>
2022-10-11 02:06:26 -0300

    Adiciona texto de quantidade e recebe aprovação do requisito 8

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -7,11 +7,16 @@ export default class CartItem extends Component {
     return (
       <>
         <img src={ thumbnail } alt={ title } />
-        <span>
+        <span data-testid="shopping-cart-product-name">
           {title}
         </span>
         <span>
           {price}
+        </span>
+        <span
+          data-testid="shopping-cart-product-quantity"
+        >
+          Quantidade: 1
         </span>
         <span>
           ID:
